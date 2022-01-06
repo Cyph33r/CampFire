8a95c32e010076a2af9c69c8f363aec175d38874
Ajai Olufisayo <tadeajai4@gmail.com>
2022-01-06 12:11:37 +0100

    Added message sending

--- app/src/main/java/com/cyphertech/games/campfire/ChatActivity.kt
+++ app/src/main/java/com/cyphertech/games/campfire/ChatActivity.kt
@@ -3,16 +3,34 @@ package com.cyphertech.games.campfire
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cyphertech.games.campfire.databinding.ActivityChatBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class ChatActivity : AppCompatActivity() {
     private var _activityChatBinding: ActivityChatBinding? = null
     private val activityChatBinding: ActivityChatBinding
         get() = _activityChatBinding!!
+    private val mAuth = FirebaseAuth.getInstance()
+    private val db = Firebase.firestore
+    private lateinit var  receiverUid:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _activityChatBinding = ActivityChatBinding.inflate(layoutInflater)
         val chatIntent = intent
-        val receiverUid = chatIntent.getStringExtra(EXTRA_RECEIVER_UID)
+        receiverUid = chatIntent.getStringExtra(EXTRA_RECEIVER_UID)!!
         setContentView(activityChatBinding.root)
+        activityChatBinding.buttonSend.setOnClickListener {
+            val messageBody = activityChatBinding.editTextTextMultiLine.text.toString()
+            val messageMeta = mapOf(
+                KEY_MESSAGE_SENDER to mAuth.currentUser!!.uid,
+                KEY_MESSAGE_RECEIVER to receiverUid,
+                KEY_MESSAGE_BODY to messageBody
+            )
+            val messagesCollection = db.collection(DB_MESSAGES_PATH)
+            messagesCollection.add(messageMeta)
+
+
+        }
     }
 }
--- app/src/main/java/com/cyphertech/games/campfire/Constants.kt
+++ app/src/main/java/com/cyphertech/games/campfire/Constants.kt
@@ -2,6 +2,10 @@ package com.cyphertech.games.campfire
 
 const val KEY_UID = "uid"
 const val KEY_USER_DISPLAY_NAME = "user_display_name"
-const val KEY_USER_EMAIL =  "user_email"
+const val KEY_USER_EMAIL = "user_email"
+const val KEY_MESSAGE_BODY = "body"
+const val KEY_MESSAGE_SENDER = "sender"
+const val KEY_MESSAGE_RECEIVER = "receiver"
 const val DB_USER_COLLECTION_PATH = "users"
+const val DB_MESSAGES_PATH = "messages"
 const val EXTRA_RECEIVER_UID = "extra_uid"
